828ba1acd9b64845efc4e606f654b80b3cb352b0
apet2929 <realandrewpetersen@gmail.com>
2021-10-19 16:28:52 -0500

    Added  EnemySpawner

--- game.py
+++ game.py
@@ -11,8 +11,13 @@ import pygame.mouse
 import pygame.transform
 import pygame.sprite
 import pygame.time
+import pygame.event
 
 # This file is where the main game is going to be stored
+
+# TODO: Add more enemy types
+class EnemyType(Enum):
+    BASIC = 0
 
 class Tower(Sprite):
     towers = Group()
@@ -117,22 +122,74 @@ class Bullet(Sprite):
         # Returns bullet just in case we need it, in most cases we don't
         return bullet
 
+class EnemySpawner():
+    """
+    send_next_event tells the game to send the next enemy
+    round_over_event tells the game that the round is over
+    """
+    next_enemy_event = pygame.USEREVENT + 1
+    round_over_event = pygame.USEREVENT + 2
+
+    def __init__(self, enemyList, delayList):
+        """
+        self.enemies is a list of Enemy, each enemy in the list has a corresponding value in the delayList
+        the value in the delayList represents the amount of time to be waited before spawning the next enemy
+
+        IMPORTANT: enemyList must be the same length as delayList
+        """
+    
+        self.enemyList: list[Enemy] = enemyList
+        self.delayList: list[float] = delayList
+    def start(self):
+        """pygame.USEREVENT + 1 is going to say 'Spawn the next enemy' basically"""
+        pygame.time.set_timer(EnemySpawner.next_enemy_event, self.delayList[0])
+        print("Starting round")
+    
+    def spawnNextEnemy(self):
+        """
+        Removes the first enemy from EnemySpawner list and adds it to the enemies group,
+        Creates a timer to spawn the next one
+        """
+        print("Spawning next enemy")
+        Enemy.enemies.add(self.enemyList.pop(0))
+
+        # No more enemies left
+        if len(self.enemyList) == 0:
+            pygame.event.post(pygame.event.Event(EnemySpawner.round_over_event))
+        else:
+            # The 1 in set_timer tells the program not to loop
+            pygame.time.set_timer(EnemySpawner.next_enemy_event, self.delayList.pop(0), 1)
+
 
 # TODO: Have enemies follow bezier curve
 class Enemy(Sprite):
     enemies = Group()
     path = list[Vector2] #  The path the enemies will follow
-    def __init__(self, image): 
+    def __init__(self, image, type): 
         super().__init__()
+        self.type = type
+        # TODO: have list of images, 1 per enemy type, and have enemy type decide the image
         self.image = image
+        
         self.rect = image.get_rect()
         self.rect.x = Enemy.path[0].x
         self.rect.y = Enemy.path[1].y
-        self.time_alive = 0
-        self.health = 100
 
-        # After how long will the enemy reach the end of the path
-        self.max_time = 10
+        """
+        time_alive is used to track how long the enemy has been alive and directly corresponds to its position on the map
+        """
+        self.time_alive = 0
+        
+        """
+        type corresponds to the EnemyType Enum
+        It determines the sprite, speed, health, and any other attributes that I decide
+        """
+        if self.type == EnemyType.BASIC:
+            self.health = 100
+            self.max_time = 10
+        else:
+            self.health = 10
+            self.max_time = 20
 
     def update(self, delta):
         """
@@ -160,8 +217,6 @@ class Enemy(Sprite):
         bullet.kill()
 
 
-
-
 def mainGame(FPS, clock, screen, images):
     running = True
     
@@ -172,9 +227,25 @@ def mainGame(FPS, clock, screen, images):
         Vector2(SCREEN_WIDTH, SCREEN_HEIGHT)
     ]
     Enemy.path = mapPath
-    Enemy.enemies.add(Enemy(images[1]))
+
+    # Add test enemy
+    Enemy.enemies.add()
     Bullet.images.append(images[0]) 
+
+    # Add test tower
     Tower.towers.add(Tower(images[1], (500,300)))
+
+    spawner = EnemySpawner(
+        enemyList=[
+        Enemy(images[1], EnemyType.BASIC),
+        Enemy(images[1], EnemyType.BASIC),
+        Enemy(images[1], EnemyType.BASIC)
+        ],
+        delayList=[
+            500, 2000, 50
+        ]
+    )
+    pygame.time.set_timer(pygame.USEREVENT + 3, 500, 1)
 
     while running:
         delta = clock.tick(FPS) / 1000
@@ -184,12 +255,19 @@ def mainGame(FPS, clock, screen, images):
             if event.type == pygame.QUIT:
                 return GameState.QUIT
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                # Creates a bullet and adds it to the bullet group
-                mouseX, mouseY = pygame.mouse.get_pos()
-                Bullet.genBullet(images[0], Vector2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2), Vector2(mouseX, mouseY))
-            
-        # screen.blit(images[0], (0,0))
+            elif event.type == pygame.MOUSEBUTTONUP:
+                # Temporary, start the round on mousebuttonup
+                spawner.start()
+            elif event.type == EnemySpawner.next_enemy_event:
+                spawner.spawnNextEnemy()
+            elif event.type == EnemySpawner.round_over_event:
+                # Round over, do stuff here
+                print("No more enemies")
+                pass
+                
+            elif event.type == pygame.USEREVENT + 3:
+                print("yee test")
+
         Tower.towers.update(delta)
         Enemy.enemies.update(delta)
         Bullet.bullets.update(delta)
